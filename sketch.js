1c2bcac8dbc5f6963a71ff0ecb04d9dd0483882a
Gabriel-BRC <99754689+Gabriel-BRC@users.noreply.github.com>
2022-04-04 13:24:51 -0300

    04/04/22
    Na aula 20, você aprendeu a criar vários tipos de formas usando um motor de física. Também aprendeu a adicionar as propriedades físicas a esses objetos e observar seu comportamento. Neste projeto, você vai praticar e aplicar os mesmos conceitos para criar uma bola que cai usando um jogo de motor de física.
    
    O irmão mais novo da Karine adora jogar jogos simples envolvendo Física. Ela está aprendendo a programar e quer surpreender seu irmão mais novo com um jogo — o Formas Cadentes. Como ela acabou de começar sua jornada com motores de física, vai precisar da sua ajuda para completar o jogo.
    
    Você pode ajudá-la a criar o jogo?

---
+++ sketch.js
@@ -0,0 +1,94 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var blocks1
+var blocks2
+var blocks3
+var chao
+
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+engine = Engine.create();
+	world = engine.world;
+
+  var blocks1_options = {
+restitution:0.5,
+friction:0.02,
+frictionAir:0
+}
+var blocks2_options = {
+restitution: 0.7,
+friction:0.01,
+frictionAir:0.1
+}
+var blocks3_options = {
+restitution:0.01,
+friction:1,
+frictionAir:0.3
+}
+var chao_options = {
+  isStatic : true
+
+}
+
+
+  
+
+	
+	//Crie os Corpos Aqui.
+blocks1 = Bodies.circle(220,10,10,blocks1_options);
+World.add(world,blocks1);
+
+blocks2 = Bodies.rectangle(110,50,10,10,blocks2_options);
+
+World.add(world,blocks2);
+
+blocks3 = Bodies.rectangle(350,50,10,10,blocks3_options);
+World.add(world,blocks3);
+
+chao = Bodies.rectangle(300, 600, 500, 20,chao_options);
+World.add(world,chao);
+  
+
+rectMode(CENTER);
+ellipseMode(RADIUS);
+
+}
+
+
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+
+  ellipse(blocks1.position.x,blocks1.position.y,10);
+  
+
+  rect(blocks2.position.x,blocks2.position.y,10,10);
+ 
+
+  rect(blocks3.position.x,blocks3.position.y,10,10);
+ rect(chao.position.x,chao.position.y,500, 20);
+
+
+
+
+
+
+
+  
+ drawSprites();
+}
+
+
+
